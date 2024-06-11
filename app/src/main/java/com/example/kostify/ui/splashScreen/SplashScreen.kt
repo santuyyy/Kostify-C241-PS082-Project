fc07269110cd1e6cebc1905efb8e7109e6faf342
Johanes Baptista <165165571+Johns26@users.noreply.github.com>
2024-06-11 15:07:27 +0700

    mengganti id & icon apps

--- app/src/main/java/com/example/kostify/ui/splashScreen/SplashScreen.kt
+++ app/src/main/java/com/example/kostify/ui/splashScreen/SplashScreen.kt
@@ -1,21 +1,34 @@
 package com.example.kostify.ui.splashScreen
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.kostify.R
+import com.example.kostify.ui.loginRegisterPage.LoginActivity
+import com.example.kostify.ui.onboardingPage.OnboardingActivity
 
 class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_splash_screen)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        supportActionBar?.hide()
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            goToMainActivity()
+        }, 2000)
+
+    }
+
+    private fun goToMainActivity() {
+        Intent(this, OnboardingActivity::class.java).also{
+            startActivity(it)
+            finish()
         }
     }
 }
