f27501ec5098ec87ede3abedb302ef540c8a1aed
Johanes Baptista <165165571+Johns26@users.noreply.github.com>
2024-06-15 15:49:49 +0700

    Animasi Onboarding

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.firebase.auth.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
